c5625f2fcee67cfacee085a5de8849e27f86c992
Max Schanner <arthus@gfz-potsdam.de>
2024-06-13 10:25:44 +0200

    Use _at_knots variables directly.

--- src/summarize.py
+++ src/summarize.py
@@ -5,15 +5,7 @@ import arviz as az
 from common import (
     n_coeffs,
     knots,
-    prior_mean,
-    chol,
-    n_ref,
-    ref_coeffs,
     knots_solar,
-    prior_mean_solar,
-    chol_solar,
-    n_ref_solar,
-    ref_solar,
 )
 
 try:
@@ -28,22 +20,10 @@ iData = az.InferenceData.from_netcdf(
 summary = az.summary(iData)
 summary.to_csv(f'../dat/{prefix}_summary.csv')
 
-g_cent = np.array(
-    iData.posterior['g_cent'],
-).reshape(-1, n_coeffs, len(knots)-n_ref)
-
-coeffs = np.einsum(
-    'ijk, ...ik -> ij...',
-    chol,
-    g_cent,
-) + prior_mean[:, :, None]
-
-coeffs = np.hstack(
-    (
-        coeffs,
-        np.repeat(ref_coeffs[:, :, None], coeffs.shape[2], axis=2),
-    )
-)
+coeffs = np.array(
+    iData.posterior['gs_at_knots'],
+).reshape(-1, n_coeffs, len(knots))
+coeffs = coeffs.transpose(1, 2, 0)
 
 if 'arch' in prefix:
     np.savez(
@@ -54,27 +34,14 @@ if 'arch' in prefix:
     exit()
 
 
-sm_cent = np.array(
-    iData.posterior['sm_cent'],
-).reshape(-1, len(knots_solar)-n_ref_solar)
-
-sm = np.einsum(
-    'ij, ...j -> i...',
-    chol_solar,
-    sm_cent,
-) + prior_mean_solar[:, None]
-
-sm = np.vstack(
-    (
-        sm,
-        np.repeat(ref_solar[:, None], sm.shape[1], axis=1),
-    )
-)
+sm = np.array(
+    iData.posterior['sm_at_knots'],
+).reshape(-1, len(knots_solar))
 
 np.savez(
     f'../dat/{prefix}_ensemble.npz',
     knots=knots,
     coeffs=coeffs,
     knots_solar=knots_solar,
-    solar=sm,
+    solar=sm.T,
 )
