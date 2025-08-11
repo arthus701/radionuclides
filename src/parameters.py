4d05159396b7ed6c0a2452a803f7cacbd9f130aa
Maximilian Schanner <arthus@gfz-potsdam.de>
2025-08-11 11:00:26 +0200

    Include parameter files for different 'experiments'

--- src/parameters.py
+++ src/parameters.py
@@ -1,2 +1,4 @@
 # change this line to use your own parameters
-from default_parameters import *
+# from default_parameters import *
+from longterm_parameters import *
+# from calib_fix_parameters import *
