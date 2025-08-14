26d315e5befd53229904ffeb5de54bb0b9f49ccb
Maximilian Schanner <arthus@gfz-potsdam.de>
2025-08-14 12:57:22 +0200

    Change parameters back after checking longer correlation time.

--- src/parameters.py
+++ src/parameters.py
@@ -1,5 +1,5 @@
 # change this line to use your own parameters
 from default_parameters import *
-# from longterm_parameters import *
 # from calib_fix_parameters import *
+# from longterm_parameters import *
 # from periodic_parameters import *
