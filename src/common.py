46e614bf2f78e15636dd85796b69ed581da59d7c
Maximilian Schanner <arthus@gfz-potsdam.de>
2026-05-20 16:39:28 +0200

    Try lower errors and no moving average.

--- src/common.py
+++ src/common.py
@@ -183,8 +183,9 @@ annual_C14_data = pd.read_excel(
     + 'c14_prod_PMIP_A-B-D.xlsx',
 )
 annual_C14_data['t'] = 1950 - annual_C14_data['Years BP']
-annual_C14_data['C14'] = annual_C14_data['c14_D']
-annual_C14_data['dC14'] = annual_C14_data['sigma_c14_D']
+annual_C14_data['C14'] = annual_C14_data['c14_B']
+# annual_C14_data['dC14'] = annual_C14_data['sigma_c14_B']
+annual_C14_data['dC14'] = 0.01
 
 # annual_C14_data['t'] = 1950 + annual_C14_data['age -yr BP']
 # annual_ensemble = annual_C14_data.values[:, 5:-1]
@@ -195,7 +196,7 @@ annual_C14_data['dC14'] = annual_C14_data['sigma_c14_D']
 # annual_C14_data['C14'] = annual_C14_data.values[:, 1]
 # annual_C14_data['dC14'] = annual_C14_data.values[:, 2]
 
-annual_C14_data['C14'] = moving_average(annual_C14_data, 2)
+# annual_C14_data['C14'] = moving_average(annual_C14_data, 2)
 
 # annual_C14_data = annual_C14_data[annual_C14_data['t'] > t_solar_fine]
 
